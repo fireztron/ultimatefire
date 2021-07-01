3a8f5903e65eb881f57a17c3025e03a9e2f9233a
fireztron <84250829+fireztron@users.noreply.github.com>
2021-07-01 19:36:29 -0400

    Create scrape scripts.lua

---
+++ scrape scripts.lua
@@ -0,0 +1,32 @@
+--// Log text function
+local function logText(fileName, txt)
+    if isfile(fileName) then
+        appendfile(fileName, txt)
+    else
+        writefile(fileName, txt)
+    end
+end
+
+warn("hi")
+local first = false
+for i,v in pairs(game:GetDescendants()) do
+    if 
+        v.Name ~= "Animate" and (v:IsA("LocalScript") or v:IsA("ModuleScript")) 
+        and not v:IsDescendantOf(game:GetService("StarterPlayer"))
+        and not v:IsDescendantOf(game:GetService("StarterGui"))
+        and not v:IsDescendantOf(game:GetService("Players").LocalPlayer.PlayerScripts.PlayerModule)
+        and not v:IsDescendantOf(game:GetService("Players").LocalPlayer.PlayerScripts.ChatScript)
+        and not v:IsDescendantOf(game:GetService("CorePackages")) 
+        and not v:IsDescendantOf(game:GetService("CoreGui")) 
+        and not v:IsDescendantOf(game:GetService("Chat")) 
+    then
+        if (v.Name == "ResetGui" and not first) or v.Name ~= "ResetGui" then
+            local txt = tostring(decompile(v))
+            txt = txt .. "\n" .. v:GetFullName() .. "\n"
+            logText("penis", txt)
+            first = true
+        end
+    end
+end
+
+warn("AHHH")
